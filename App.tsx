086875e9a50aab6b1adfbe3340029fd2a5d75396
afrianska <afrianska@gmail.com>
2024-03-09 21:09:34 +0700

    begin sengon stack

--- App.tsx
+++ App.tsx
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.headerSengon}>SENGON STACK</Text>
+      <Text style={styles.textSengon}>A minimal React-Native stack to serve as a starting point for demos.</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#200509',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+    marginTop: 24
   },
+  headerSengon: {
+    color: '#F43F5E',
+    fontSize: 32,
+    textAlign: 'center',
+    marginBottom: 24,
+    fontFamily: 'Inter',
+    fontWeight: '700'
+  },
+  textSengon: {
+    color: '#FECDD3',
+    fontSize: 18,
+    textAlign: 'center',
+    fontFamily: 'Inter',
+    fontWeight: '300'
+  }
 });
---
+++ README.md
@@ -0,0 +1,12 @@
+# RN Sengon Stack
+
+This is a minimal React-Native stack to serve as a starting point for demos. Learn more about [React-Native](https://reactnative.dev/).
+
+```
+npx create-expo-app --template afrianska/sengon-stack
+```
+
+## What's in the stack
+
+- [React-Native](https://reactnative.dev/)
+- [Expo](https://expo.dev/)
